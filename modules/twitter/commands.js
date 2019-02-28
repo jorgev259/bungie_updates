39753693b9b76be3f655a8aabcb5691f2bfe05f0
Jorge Vargas <jorgev259@outlook.es>
2019-02-28 15:46:42 -0600

    change conditions

--- modules/twitter/commands.js
+++ modules/twitter/commands.js
@@ -3,7 +3,7 @@ module.exports.commands = {
     usage: 'Usage: change #channel-name',
     desc: 'Changes the channel where the tweets are sent',
     async execute (client, msg, param, db) {
-      if (!param[1] || msg.mentions.channels.size === 0 || !msg.guild.channels.some(c => c.name === param[1].toLowerCase())) return msg.channel.send('Invalid channel name')
+      if (!param[1] || (msg.mentions.channels.size === 0 && !msg.guild.channels.some(c => c.name === param[1].toLowerCase()))) return msg.channel.send('Invalid channel name')
       let name
 
       if (msg.mentions.channels.size > 0) name = msg.mentions.channels.first().name
