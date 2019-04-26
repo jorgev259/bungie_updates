bd70ae61e96f047fe0f4509775b6630f41dd7c9b
Jorge Vargas <jorgev259@outlook.es>
2019-04-26 13:25:30 -0600

    rework reddit module

--- modules/reddit/events.js
+++ modules/reddit/events.js
@@ -2,6 +2,7 @@ const { log } = require('../../utilities.js')
 const config = require('../../data/config.js')
 var snoowrap = require('snoowrap')
 const twit = require('twit')(config.twitter)
+var twitter = require('twitter-text')
 
 const r = new snoowrap(config.reddit)
 
@@ -64,5 +65,63 @@ function refresh (run) {
 }
 
 async function post (comment, item) {
-  twit.post('statuses/update', { status: `${comment.link_title} (Reply by ${item.handle})\n${comment.body} ${comment.link_permalink}` })
+  let title = `${comment.link_title}`
+  let context = [`${comment.parent_id.startsWith('t1') ? `"${await r.getComment(comment.parent_id.split('_')[1]).body}"\n` : ''}`]
+  let url = ` https://reddit.com${comment.permalink}`
+  let body = `(Reply by ${item.handle}): ${comment.body}`
+
+  let parse = twitter.txt.parseTweet(`${title}\n${context}${body}${url}`)
+  if (parse.valid) twit.post('statuses/update', { status: `${title}\n${context}${body}${url}` })
+  else {
+    let parts = [`${title}\n${context}`, body, url]
+    let finalParts = []
+    /* while (!parts.every(function (e, i) {
+      let text = i > 0 ? `@UpdatesVanguard ${e}` : e
+      return twitter.txt.parseTweet(text).valid
+    })) {
+      let newParts = []
+      for (var i = 0; i < parts.length; i++) {
+        let e = parts[i]
+        let text = i > 0 ? `@UpdatesVanguard ${e}` : e
+
+        let parsePart = twitter.txt.parseTweet(text)
+        if (parsePart.valid) newParts.push(e)
+        else {
+
+        }
+      }
+
+      parts = newParts
+    } */
+    let text = ''
+    for (var i = 0; i < parts.length; i++) {
+      let testText = text + parts[i]
+      if (finalParts.length > 0) testText = `@UpdatesVanguard ${testText}`
+
+      let parsePart = twitter.txt.parseTweet(testText)
+      if (parsePart.valid) text = text + parts[i]
+      else {
+        finalParts.push(text)
+        if (i === parts.length - 1) finalParts.push(parts[i])
+        else text = parts[i]
+      }
+    }
+
+    console.log(finalParts)
+    twit.post('statuses/update', { status: finalParts[0] }).then(res => {
+      let { data } = res
+      finalParts.shift()
+      nextReply(finalParts, data.id_str)
+    })
+  }
+}
+
+function nextReply (parts, id) {
+  twit.post('statuses/update', { status: `@UpdatesVanguard ${parts[0]}`, in_reply_to_status_id: id }).then(res => {
+    let { data } = res
+    if (parts.length > 1) {
+      parts.shift()
+      nextReply(parts, data.id_str)
+    }
+  })
 }
