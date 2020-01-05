e09be3432e0dfc3802ea20f661d87469b61b5bdb
jorgev259 <jorgev259@outlook.es>
2020-01-04 22:16:21 -0600

    use correct config

--- modules/twitter/util.js
+++ modules/twitter/util.js
@@ -1,10 +1,11 @@
 module.exports = {
   broadcast: function (client, db, content) {
+    const { defaultChannel } = client.data['lotus_config.twitter']
     client.guilds.forEach(guild => {
       try {
         // let { name } = db.prepare('SELECT name FROM tweetChannels WHERE guild=?').get(guild.id)
-        var channel = db.prepare('SELECT value FROM config WHERE guild=? AND type=?').get(guild.id, 'twitter_channel').value
-        guild.channels.find(c => c.name === channel).send(content)
+        // var channel = db.prepare('SELECT value FROM config WHERE guild=? AND type=?').get(guild.id, 'twitter_channel').value
+        guild.channels.find(c => c.name === defaultChannel).send(content)
       } catch (err) {
         console.log(err)
       }
